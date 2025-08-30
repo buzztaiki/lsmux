a11c38ac3c5fdc524804c6e631d7c1dcd81b73d1
Taiki Sugawara <buzz.taiki@gmail.com>
2025-08-30 16:52:37 +0900

    execute command

--- client_handler.go
+++ client_handler.go
@@ -5,8 +5,10 @@ import (
 	"encoding/json"
 	"errors"
 	"log/slog"
+	"slices"
 
 	"dario.cat/mergo"
+	"github.com/myleshyson/lsprotocol-go/protocol"
 	"golang.org/x/exp/jsonrpc2"
 )
 
@@ -21,6 +23,7 @@ type serverConn struct {
 	name string
 	*jsonrpc2.Connection
 	supportedCaps map[string]struct{}
+	caps          *protocol.ServerCapabilities
 }
 
 func NewClientHandler(nservers int) *ClientHandler {
@@ -32,7 +35,7 @@ func NewClientHandler(nservers int) *ClientHandler {
 
 func (h *ClientHandler) AddServerConn(name string, conn *jsonrpc2.Connection) {
 	if len(h.serverConns) < h.nservers {
-		h.serverConns = append(h.serverConns, &serverConn{name, conn, nil})
+		h.serverConns = append(h.serverConns, &serverConn{name, conn, nil, nil})
 	}
 	if len(h.serverConns) == h.nservers {
 		close(h.ready)
@@ -47,53 +50,36 @@ func (h *ClientHandler) Handle(ctx context.Context, r *jsonrpc2.Request) (any, e
 
 	<-h.ready
 
-	switch r.Method {
-	case "initialize":
-		var merged map[string]any
-		for _, conn := range h.serverConns {
-			var res map[string]any
-			if err := conn.Call(ctx, r.Method, r.Params).Await(ctx, &res); err != nil {
+	method := protocol.MethodKind(r.Method)
+	if method == protocol.InitializeMethod {
+		return h.handleInitializeRequest(ctx, r, logger)
+	}
+
+	serverConns := []*serverConn{}
+	for _, conn := range h.serverConns {
+		if IsMethodSupported(r.Method, conn.supportedCaps) {
+			serverConns = append(serverConns, conn)
+		}
+	}
+
+	if len(serverConns) == 0 {
+		return nil, ErrMethodNotFound
+	}
+
+	if !r.IsCall() {
+		for _, conn := range serverConns {
+			if err := conn.Notify(ctx, r.Method, r.Params); err != nil {
 				return nil, err
 			}
-
-			caps, ok := res["capabilities"].(map[string]any)
-			if !ok {
-				return nil, errors.New("no capabilities in initialize response")
-			}
-
-			mergo.Merge(&merged, caps)
-			conn.supportedCaps = CollectSupportedCapabilities(caps)
-			logger.Info("supported server capabilities", "server", conn.name, "capabilities", conn.supportedCaps)
 		}
+		return nil, nil
+	}
 
-		return map[string]any{
-			"serverInfo": map[string]any{
-				"name": "lspmux", // TODO configurable
-			},
-			"capabilities": merged,
-		}, nil
+	switch method {
+	case protocol.WorkspaceExecuteCommandMethod:
+		return h.handleExecuteCommandRequest(ctx, r, serverConns, logger)
 
 	default:
-		serverConns := []*serverConn{}
-		for _, conn := range h.serverConns {
-			if IsMethodSupported(r.Method, conn.supportedCaps) {
-				serverConns = append(serverConns, conn)
-			}
-		}
-
-		if len(serverConns) == 0 {
-			return nil, ErrMethodNotFound
-		}
-
-		if !r.IsCall() {
-			for _, conn := range serverConns {
-				if err := conn.Notify(ctx, r.Method, r.Params); err != nil {
-					return nil, err
-				}
-			}
-			return nil, nil
-		}
-
 		var res json.RawMessage
 		// Currently, request is sent to the first server only
 		// TODO Some methods should have their results merged
@@ -105,4 +91,64 @@ func (h *ClientHandler) Handle(ctx context.Context, r *jsonrpc2.Request) (any, e
 		return res, nil
 
 	}
+
+}
+
+func (*ClientHandler) handleExecuteCommandRequest(ctx context.Context, r *jsonrpc2.Request, serverConns []*serverConn, logger *slog.Logger) (any, error) {
+	var params protocol.ExecuteCommandParams
+	if err := json.Unmarshal(r.Params, &params); err != nil {
+		return nil, err
+	}
+
+	for _, conn := range serverConns {
+		if slices.Index(conn.caps.ExecuteCommandProvider.Commands, params.Command) == -1 {
+			continue
+		}
+
+		logger.Info("executeCommand", "command", params.Command, "server", conn.name)
+		var res json.RawMessage
+		// TODO use params instead of r.Params?
+		if err := conn.Call(ctx, r.Method, r.Params).Await(ctx, &res); err != nil {
+			return nil, err
+		}
+		return res, nil
+	}
+
+	return nil, ErrMethodNotFound
+}
+
+func (h *ClientHandler) handleInitializeRequest(ctx context.Context, r *jsonrpc2.Request, logger *slog.Logger) (any, error) {
+	var merged map[string]any
+	for _, conn := range h.serverConns {
+		var rawRes json.RawMessage
+		if err := conn.Call(ctx, r.Method, r.Params).Await(ctx, &rawRes); err != nil {
+			return nil, err
+		}
+
+		var res protocol.InitializeResult
+		if err := json.Unmarshal(rawRes, &res); err != nil {
+			return nil, err
+		}
+
+		var kvRes map[string]any
+		if err := json.Unmarshal(rawRes, &kvRes); err != nil {
+			return nil, err
+		}
+
+		kvCaps, ok := kvRes["capabilities"].(map[string]any)
+		if !ok {
+			return nil, errors.New("no capabilities in initialize response")
+		}
+
+		mergo.Merge(&merged, kvCaps)
+		conn.caps = &res.Capabilities
+		conn.supportedCaps = CollectSupportedCapabilities(kvCaps)
+	}
+
+	return map[string]any{
+		"serverInfo": map[string]any{
+			"name": "lspmux", // TODO configurable
+		},
+		"capabilities": merged,
+	}, nil
 }
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.6
 require (
 	dario.cat/mergo v1.0.2
 	github.com/goccy/go-yaml v1.18.0
+	github.com/myleshyson/lsprotocol-go v1.0.2
 	golang.org/x/exp/jsonrpc2 v0.0.0-20250819193227-8b4c13bb791b
 )
 
